9a847e05d0e99245dc53ae03f536f7ce93f61df0
Taj Smith <TajSmith@Deloitte.com>
2024-03-13 15:44:55 -0400

    Created employees class

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const { SalesPerson } = require("./classes/SalesPerson")
 const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
 
 // Run and debug your code here
-
-
+const preston = new Employees("Preston", "Engineer", 100000);
+console.log(preston.getSalary()); // 100000
+preston.setSalary(105000);
+console.log(preston.getSalary()); // 105000
+console.log(preston.getStatus()); // true;
+preston.setStatus("fire");
+console.log(preston.getStatus()); // false;
 
 
 
